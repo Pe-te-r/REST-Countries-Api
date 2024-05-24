587ece57cdbd22cf89a5ac8f8e96a58906836a70
Pe-te-r <mburupeter8526@gmail.com>
2024-05-24 16:51:04 +0300

    for more info

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// changing theme settings
+  function changeTheme(){
+    document.body.classList.toggle('darkMode')
+  }
 
 // importing the json file from data.json
 async function countryInformation() {
@@ -22,21 +26,21 @@ async function countryInformation() {
 //  handles displaying container for each country individually when country is passed.
   function displayCountry(data){
     const mainContainer=document.getElementById('main')
-    // mainContainer.innerHTML='';
 
     const mainDiv=document.createElement('div');
     mainDiv.classList.add('container');
-      
+
+    
     const imageDiv=document.createElement('div');
     imageDiv.classList.add('image');
     const img=document.createElement('img');
     flag_image=data.flags['png']
     img.src=flag_image;
     imageDiv.appendChild(img);
-
+    
     const infoDiv=document.createElement('div');
     infoDiv.classList.add('info');
-
+    
     const h3=document.createElement('h3');
     // console.log(data.name)
     h3.innerText=data.name
@@ -45,36 +49,63 @@ async function countryInformation() {
     contryInfo.classList.add('contry-info');
 
     const population=document.createElement('p');
-    population.innerHTML=`<span>Population:</span> <span>${data.population}</span>`;
+    population.innerHTML=`<span class="bold">Population:</span> <span class="faint">${data.population}</span>`;
     contryInfo.appendChild(population);
-
+    
     const region=document.createElement('p');
-    region.innerHTML=`<span>Region:</span> <span>${data.region}</span>`;
+    region.innerHTML=`<span class="bold">Region:</span> <span class="faint">${data.region}</span>`;
     contryInfo.appendChild(region);
-
+    
     const capital=document.createElement('p');
-    capital.innerHTML=`<span>Capital:</span> <span>${data.capital}</span>`;
+    capital.innerHTML=`<span class="bold">Capital:</span> <span class="faint">${data.capital}</span>`;
     contryInfo.appendChild(capital);
-
+    
     infoDiv.appendChild(h3);
     infoDiv.appendChild(contryInfo);
+    
+    infoDiv.addEventListener('click', ()=>{
+      const countryClicked=infoDiv.querySelector('h3')
+      showMoreInfo(countryClicked.innerHTML)
+      // console.log(countryClicked)
+    })
 
     mainDiv.appendChild(imageDiv);
     mainDiv.appendChild(infoDiv);
-
+    
     mainContainer.appendChild(mainDiv);
-
+    
   }
 
-
-
-function displayCountries(contriesData, selectedContinent) {
- 
+// It will render the country information in much more details.
+async function showMoreInfo(country_received){
+  // console.log(country)
+  const countries=await countryInformation()
+  const selected_country=countries.find(country => country.name === country_received)
+  if(selected_country !== undefined){
     const mainContainer=document.getElementById('main')
+    const search =document.getElementById('search')
+    search.style.display="none"
     mainContainer.innerHTML='';
+
+    const mainEntry=document.createElement('div')
+    mainEntry.classList.add('mainEntry')
+
+    mainContainer.appendChild(mainEntry)
+
+
+  }
+  console.log(selected_country)
+
+}
+
+
+  // this function receives the json for all countries and the selected continent
+  function displayCountries(contriesData, selectedContinent) {
+ 
+    
     contriesData.forEach((countryInfo) => {
         if (countryInfo.region === selectedContinent){
-            console.log(countryInfo.name)
+            // console.log(countryInfo.name)
             displayCountry(countryInfo);
         }
     });
@@ -82,7 +113,7 @@ function displayCountries(contriesData, selectedContinent) {
   
 
 
-// display for each country
+// display for each continent based on the dropdown
 const filterDropdown = document.getElementById('filter-dropdown');
 filterDropdown.addEventListener('change', () => {
     const selectedContinent = filterDropdown.value;
@@ -90,7 +121,16 @@ filterDropdown.addEventListener('change', () => {
     (async () => {
       try {
         const countriesData = await countryInformation();
-        displayCountries(countriesData, selectedContinent); 
+        const mainContainer=document.getElementById('main')
+        mainContainer.innerHTML='';
+
+        if (selectedContinent.toLowerCase()==='all'){
+          countriesData.forEach(country => {
+            displayCountry(country)
+          })
+        }else{
+          displayCountries(countriesData, selectedContinent); 
+        }
       } catch (error) {
         console.error('Error fetching country information:', error);
       }
@@ -113,8 +153,11 @@ countrySearch.addEventListener('keydown', (event) => {
                     mainContainer.innerHTML='';
                     console.log(selectedCountry);
                     displayCountry(selectedCountry);
+                    countrySearch.value = '';
                 } else {
                     console.log(`No country found with the name "${inputValue}"`);
+                    alert(`No country found with the name "${inputValue}"`);
+                    countrySearch.value = '';
                 }
             } catch (error) {
                 console.error('Error fetching country information:', error);
@@ -122,22 +165,6 @@ countrySearch.addEventListener('keydown', (event) => {
         })();
     }
 });
-
-
-
-
-
-
-
-// handle one country check for a specific country
-// function countryCheck(countries, country){
-//     countries.forEach((country) => {
-//         if (country.name === country){
-//             console.log(country)
-//             displayCountry(country);
-//         }
-//     })
-// }
 
 
 
@@ -157,8 +184,3 @@ countrySearch.addEventListener('keydown', (event) => {
   })();
 
   
-
-
-  function changeTheme(){
-    document.body.classList.toggle('darkMode')
-  }
